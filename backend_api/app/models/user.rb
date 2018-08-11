b17afaf07d255f664e2a083118e856ca70a1ef62
Sree Aleti <sree@aletilabs.com>
2018-08-11 23:05:51 +0000

    added backend

---
+++ backend_api/app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates :username, presence: true
+  def self.from_token_request request
+    username = request.params["auth"] && request.params["auth"]["username"]
+    self.find_by username: username
+  end
+  def to_token_payload
+        {
+            sub: id,
+            username: username,
+            role: "testing"
+        }
+   end
+end
